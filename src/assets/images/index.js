3612ea5c1c697a1b15a1872b5fbc1bb0292d95d4
Anitor <anitorabraham@gmail.com>
2023-03-27 21:47:53 +0100

    initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,43 @@
+import bg_pattern_footer_desktop from "./bg-pattern-footer-desktop.svg";
+import bg_pattern_footer_mobile from "./bg-pattern-footer-mobile.svg";
+import bg_pattern_how_we_work_desktop from "./bg-pattern-how-we-work-desktop.svg";
+import bg_pattern_how_we_work_mobile from "./bg-pattern-how-we-work-mobile.svg";
+import bg_pattern_intro_left_desktop from "./bg-pattern-intro-left-desktop.svg";
+import bg_pattern_intro_left_mobile from "./bg-pattern-intro-left-mobile.svg";
+import bg_pattern_mobile_nav from "./bg-pattern-mobile-nav.svg";
+import favicon_logo from "./favicon-32x32.png";
+import icon_affordable_prices from "./icon-affordable-prices.svg";
+import close from "./icon-close.svg";
+import facebook_logo from "./icon-facebook.svg";
+import hamburger from "./icon-hamburger.svg";
+import instagram from "./icon-instagram.svg";
+import person_logo from "./icon-people-first.svg";
+import pinterest from "./icon-pinterest.svg";
+import snappy_process from "./icon-snappy-process.svg";
+import twitter from "./icon-twitter.svg";
+import image_intro_desktop from "./image-intro-desktop.jpg";
+import image_intro_mobile from "./image-intro-mobile.jpg";
+import insure_logo from "./logo.svg";
+
+export {
+  bg_pattern_footer_desktop,
+  bg_pattern_footer_mobile,
+  bg_pattern_how_we_work_desktop,
+  bg_pattern_how_we_work_mobile,
+  bg_pattern_intro_left_desktop,
+  bg_pattern_intro_left_mobile,
+  bg_pattern_mobile_nav,
+  favicon_logo,
+  icon_affordable_prices,
+  close,
+  facebook_logo,
+  hamburger,
+  instagram,
+  person_logo,
+  pinterest,
+  snappy_process,
+  twitter,
+  image_intro_desktop,
+  image_intro_mobile,
+  insure_logo,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,163 @@
+import {
+  bg_pattern_footer_desktop,
+  bg_pattern_footer_mobile,
+  bg_pattern_how_we_work_desktop,
+  bg_pattern_how_we_work_mobile,
+  bg_pattern_intro_left_desktop,
+  bg_pattern_intro_left_mobile,
+  bg_pattern_mobile_nav,
+  favicon_logo,
+  icon_affordable_prices,
+  close,
+  facebook_logo,
+  hamburger,
+  instagram,
+  person_logo,
+  pinterest,
+  snappy_process,
+  twitter,
+  image_intro_desktop,
+  image_intro_mobile,
+  insure_logo,
+} from "../assets/images";
+
+export const navLinks = [
+  {
+    id: "work",
+    title: "HOW WE WORK",
+  },
+  {
+    id: "blog",
+    title: "BLOG`",
+  },
+  {
+    id: "account",
+    title: "ACCOUNT",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: snappy_process,
+    title: "Snappy Process",
+    content:
+      "Our application process can be completed in minutes, not hours. Don’t get stuck filling in tedious forms.",
+  },
+  {
+    id: "feature-2",
+    icon: icon_affordable_prices,
+    title: "Affordable Prices",
+    content:
+      "We don’t want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "People First",
+    content:
+      "Our plans aren’t full of conditions and clauses to prevent payouts. We make sure you’re covered when you need it.",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: facebook_logo,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: pinterest,
+    link: "https://www.pinterest.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "OUR COMPANY",
+    links: [
+      {
+        name: "HOW WE WORK",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "WHY INSURE?",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "CHECK PRICE",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "REVIEWS",
+        link: "https://www.hoobank.com/explore/",
+      },
+    ],
+  },
+  {
+    title: "HELP ME",
+    links: [
+      {
+        name: "FAQ",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "TERMS OF USE",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "PRIVACY POLICY",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "COOKIES",
+        link: "https://www.hoobank.com/blog/",
+      },
+    ],
+  },
+  {
+    title: "CONTACT",
+    links: [
+      {
+        name: "SALES",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "SUPPORT",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+      {
+        name: "LIVE CHAT",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+  {
+    title: "OTHERS",
+    links: [
+      {
+        name: "CAREERS",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "PRESS",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+      {
+        name: "LICENSES",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
